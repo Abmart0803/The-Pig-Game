a358a2c9154a3c2efe7fedd71051f67b653843ee
Abiola Martins <hsemartins@gmail.com>
2023-04-22 17:17:58 +0300

    Modify script.js file.

--- script.js
+++ script.js
@@ -4,22 +4,36 @@
 /* eslint-disable func-names */
 
 // Selecting the elements.
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
-// const current1El = document.getElementById('current--1');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
-// const btnNew = document.querySelector('.btn--new');
+const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-// const btnHold = document.querySelector('.btn--hold');
+const btnHold = document.querySelector('.btn--hold');
 
 // Stylling condition.
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+//function for switching the player
+const swictchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // rolling dice functionality.
 btnRoll.addEventListener('click', () => {
@@ -35,8 +49,26 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // add to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; // chnge later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player.
+    swictchPlayer();
   }
+});
+
+//holding the score
+btnHold.addEventListener('click', function () {
+  //1. Add current score to active plyer's score.
+  scores[activePlayer] += currentScore;
+  //scores[1] = scores[1] + currentScore.
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2. Check if player's score is >= 100
+
+  // finish the game
+
+  //swicth to the next player.
+  swictchPlayer();
 });
